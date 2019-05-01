b1ae30cd9b5287ed6ba96db6aed9f76d5cf1a064
PhiSch19 <50172626+PhiSch19@users.noreply.github.com>
2019-05-01 18:32:31 +0200

    Delete Utils.java

--- src/at/phisch19/tools/googlelocationconverter/Utils.java
+++
@@ -1,7 +0,0 @@
-package at.phisch19.tools.googlelocationconverter;
-
-public class Utils {
-	public static String createHTMLLink(String linkTo, String display) {
-		return "<a href=\"" + linkTo + "\" target=\"_blank\">" + display + "</a></br>";
-	}
-}
